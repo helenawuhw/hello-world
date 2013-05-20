7d67f118aa10fc6a2568c5ee6729cee768976fef
Nick Heiner <nick.heiner@opower.com>
2013-05-20 14:42:53 -0400

    Adding basic fibonacci implementation

--- p1.py
+++ p1.py
@@ -11,10 +11,19 @@ def name(value):
     print "Hello " + value
     print random.randint(1,6)
     
-    
+def fib(num):
+  if num == 0:
+    return 0
+  
+  if num == 1:
+    return 1
+
+  return fib(num - 1) + fib(num - 2)    
     
 # Application code
 
 if __name__ == "__main__":
     name("Helena")
     print "Working!"
+
+    print "fib(10) is " + `fib(10)`
