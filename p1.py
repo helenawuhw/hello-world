262d1ad419ad18691037f59149574ef14aa6c553
Helena Wu <helenawu.hw@gmail.com>
2013-05-20 13:59:56 -0400

    personal message

--- p1.py
+++ p1.py
@@ -3,3 +3,15 @@
 # 05/20
 
 print "Hello World!"
+
+
+def name(value):
+    print "Hello " + value
+    
+    
+    
+# Application code
+
+if __name__ == "__main__":
+    name("Helena")
+    print "Working!"
