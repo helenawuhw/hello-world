5a2db27e98df999e277e589453baa69698f45300
Helena Wu <helenawu.hw@gmail.com>
2013-05-20 14:22:41 -0400

    adding an initial prototype that prints out name and number

---
+++ p1.py
@@ -0,0 +1,20 @@
+# p1.py
+# Helena Wu
+# 05/20
+
+import random
+
+print "Hello World!"
+
+
+def name(value):
+    print "Hello " + value
+    print random.randint(1,6)
+    
+    
+    
+# Application code
+
+if __name__ == "__main__":
+    name("Helena")
+    print "Working!"
